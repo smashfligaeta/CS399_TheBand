035bd43efe07ada2dd764c18571c0618588617a7
Dylan Grayson <dgrayson11@gmail.com>
2015-01-16 18:50:47 -0700

    made a nav bar with some bootstrap styling

--- Project1/urls.py
+++ Project1/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+
     # Examples:
-    # url(r'^$', 'Project1.views.home', name='home'),
+    url(r'^$', 'Project1.views.home', name='home'),
+    url(r'^bio/', 'Project1.views.bio', name='bio'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
